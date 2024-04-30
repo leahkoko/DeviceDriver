5b8889890712844ad527e40b0e8e4503d2555806
leah <dmsrud6178@naver.com>
2024-04-30 14:57:25 +0900

    First Commit

---
+++ src/main/java/DeviceDriver.java
@@ -0,0 +1,21 @@
+import java.util.Map;
+
+/**
+ * This class is used by the operating system to interact with the hardware 'FlashMemoryDevice'.
+ */
+public class DeviceDriver {
+//Mock 만들어서 5번 read 후 pass 라고 unit test 만들고 시작
+    public DeviceDriver(FlashMemoryDevice hardware) {
+        // TODO: implement this method
+    }
+
+    public byte read(long address) {
+
+        // TODO: implement this method
+        return -1;
+    }
+
+    public void write(long address, byte data) {
+        // TODO: implement this method
+    }
+}
